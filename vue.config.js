f2f474ed38d27deb0099f4120896f0f2df6eb10f
xuhao <bingoxuhao@gmail.com>
2021-03-15 12:01:53 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/vue-game-1/dist/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-demo-2/dist/' : '/'
 }
